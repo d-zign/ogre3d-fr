0c6c9f08a8fcff01b1228341942ef10b74e619fa
jledentu <jledentu@gmail.com>
2011-11-01 21:45:06 +0000

    Ajout des fichiers

---
+++ wp-content/themes/ogre3d/single-user.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * User Profile
+ *
+ * @package bbPress
+ * @subpackage Theme
+ */
+
+?>
+
+<?php get_header(); ?>
+<div id="container">
+    <div class="centered">
+	<div id="content" role="main">
+
+		<div id="bbp-user-<?php bbp_current_user_id(); ?>" class="bbp-single-user">
+			<div class="entry-content">
+
+				<?php bbp_get_template_part( 'bbpress/content', 'single-user' ); ?>
+
+			</div><!-- .entry-content -->
+		</div><!-- #bbp-user-<?php bbp_current_user_id(); ?> -->
+
+	</div><!-- #content -->
+<?php get_footer(); ?>
