02f79752a085ba3d891ebf4fd369a38cebc3611a
jledentu@gmail.com <jledentu@gmail.com@ccf5fe6f-0cd8-1050-018c-9de31426bfe8>
2011-11-10 03:34:09 +0000

    Mise en forme du forum (topic) + activation des BBCodes.

--- wp-content/themes/ogre3d/bbpress/pagination-replies.php
+++ wp-content/themes/ogre3d/bbpress/pagination-replies.php
@@ -12,11 +12,11 @@
 	<?php do_action( 'bbp_template_before_pagination_loop' ); ?>
 
 	<div class="bbp-pagination">
-		<div class="bbp-pagination-count">
+		<!--<div class="bbp-pagination-count">
 
 			<?php bbp_topic_pagination_count(); ?>
 
-		</div>
+		</div>-->
 
 		<div class="bbp-pagination-links">
 
