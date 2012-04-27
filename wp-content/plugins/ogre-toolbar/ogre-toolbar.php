637f79ca802466b7d7e14a8fc7014207b12570c6
jledentu@gmail.com <jledentu@gmail.com@ccf5fe6f-0cd8-1050-018c-9de31426bfe8>
2012-04-27 13:25:34 +0000

    Désactivation des shortcodes de code pour bbpress-bbcode.

---
+++ wp-content/plugins/ogre-toolbar/ogre-toolbar.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ Plugin Name: OGRE3D.fr Toolbar
+ Plugin URI: http://www.ogre3d.fr
+ Description: A toolbar for OGRE3D.fr website.
+ Version: 0.1
+ Author: Jérémie Ledentu
+ Author URI: http://www.jeremie-ledentu.com
+*/
+
+class ogre_toolbar
+{
+
+}
+
+?>
